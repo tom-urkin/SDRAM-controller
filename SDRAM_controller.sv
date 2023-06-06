1c8a23575dcf309095750ca773730bab0863f132
tom-urkin <tom.urkin@gmail.com>
2023-06-06 10:09:15 +0300

    Source codes
    First trial - initialization and auto-refresh counter

--- SDRAM_controller.sv
+++ SDRAM_controller.sv
@@ -1,6 +1,6 @@
 //SDRAM Controller XXX version - operate with 100MHz clock
 
-module SDRAM_controller(rst,A,BA,DQ,CLK,CKE,CS_N,RAS_N,CAS_N,WE_N,DQML,DQMH);
+module SDRAM_controller(rst,,i_clk,i_initial,i_addr,i_ba,i_data,i_rw,A,BA,DQ,CLK,CKE,CS_N,RAS_N,CAS_N,WE_N,DQML,DQMH);
 
 //Parameter declerations
 parameter A_WIDTH=13;                         //SDRAM address bus length
@@ -14,6 +14,15 @@ parameter A_COL_WIDTH = 10;                   //Number of columns in each bank
 localparam POWER_DOWN=4'b0000;                //
 localparam INITIALIZATION=4'b0001;            //
 localparam WAIT=4'b0010;                      //
+localparam PRECHARGE_INIT=4'b0011;            //
+localparam AUTO_REFRESH_INIT_1=4'b0100;       //
+localparam AUTO_REFRESH_INIT_2=4'b0101;       //
+localparam MODE_REGISTER_SET=4'b0110;         //
+localparam IDLE=4'b0111;                      //
+localparam AUTO_REFRESH=4'b1000;              //
+localparam ACTIVATE=4'b1001;                  //
+localparam PRECHARGE=4'b1010;                 //
+
 
 //SDRAM Commands [CS_N,RAS_N,CAS_N,WE_N]
 localparam DESL = 4'b1000;                    //Device deselect
@@ -49,17 +58,20 @@ output logic DQML;                             //Lower byte, input/output mask
 output logic DQMH;                             //Upper byte, input/ouptut mask
 
 output logic [15:0] o_data;                    //Word read from memoy. [?] What happens here in bursts? [?]
+output logic busy;                             //Read/Write operations can be initiated only when not busy, i.e. busy==1'b0
 
 //Inouts
 inout logic [D_WIDTH-1:0] DQ;                  //
 
 //Internal signals
 logic [15:0] counter_wait;                     //
+logic [9:0] counter_rst;                       // Auto-refresh at 100MHz occurs every 782 clock cycles (8192 time every 64ms)
+logic refresh_flag;                            //
 
-//HDL code
+//HDL code : For start up: apply clock, take rst to '1'. To turn off take rst down and clock cycle later you can close the clock [!] 
 
 //Next state latching
-always @(posedge CLK or negedge rst)
+always @(posedge CLK)                                             //POWER_DOWN mode may be entered only in synch with CLK
   if (!rst)
     state<=POWER_DOWN;
   else 
@@ -68,40 +80,108 @@ always @(posedge CLK or negedge rst)
 //Next state calculation
 always @(*)
   case (state)
-    POWER_DOWN : next_state = (~rst) ? POWER_DOWN : INITIALIZATION;
+    POWER_DOWN : next_state = INITIALIZATION;
     INITIALIZATION : next_state = WAIT;
-    WAIT :
+    WAIT : next_state = (counter_wait>$bits(counter_wait)'(0)) ? WAIT: next_state_after_wait;
+    PRECHARGE_INIT : next_state = WAIT;
+    AUTO_REFRESH_INIT_1 : next_state = WAIT;
+    AUTO_REFRESH_INIT_2 : next_state = WAIT;
+    MODE_REGISTER_SET : next_State = IDLE;
+
+    //IDLE : next_state = If i need to refresh --> refresh else If I need to read/write --> activate a row else if I want to confugure the device again MRS
+    IDLE : next_state = TAKE care of the refresh first!!! [X]
+    (i_initial==1'b1) : ACT ? IDLE;
+    ACTIVATE :
  endcase
 
 //
-always @(posedge CLK or negedge rst)
-  if (!rst) begin
-    cmd<=DESL;                                                    //Deselects device to minimize power dissipation when memory is not required
+always @(posedge CLK)
+  case (state)
 
+    POWER_DOWN : begin
+      CKE<=1'b0;
+      DQML<=1'b1;
+      DQMH<=1'b1;
+      cmd<=DESL;                                                //Deselects device to minimize power dissipation when memory is not required
+    end
+
+    INITIALIZATION: begin
+      CKE<=1'b1;
+      DQML<=1'b1;
+      DQMH<=1'b1;
+      counter_wait<=$bits(counter_wait)'(10000);                 //Initialization requires 100us, i.e. 1000 cycles in 100MHz clock
+      next_state_after_wait<=PRECHARGE_INIT;
+    end
+
+    WAIT: begin
+      cmd<=NOP;
+      counter_wait<=counter_wait-$bits(counter_wait)'(1);       //WAIT state is terminated once the internal counter reaches 0
+    end
+
+    PRECHARGE_INIT: begin
+      cmd<=PRE;
+      A[10]<=1'b1;                                              //Setting A[10] to logic high precharges all banks
+      counter_wait<=$bits(counter_wait)'(2);                    //Precharge requires 2 clock cycles (PRE to ACT)
+      next_state_after_wait<=AUTO_REFRESH_INIT;
+    end
+
+    AUTO_REFRESH_INIT_1: begin
+      cmd<=REF;
+      counter_wait<=$bits(counter_wait)'(6);                    //TRC period is 60ns (REF to REF)
+      next_state_after_wait<=AUTO_REFRESH_INIT_2;               //Two auto refresh commands are required during initialization
+    end
+
+    AUTO_REFRESH_INIT_2: begin
+      cmd<=REF;
+      counter_wait<=$bits(counter_wait)'(6);                    //TRC period is 60ns (REF to REF)
+      next_state_after_wait<=MODE_REGISTER_SET;                 //Two auto refresh commands are required during initialization
+    end
+
+   MODE_REGISTER_SET: begin  //Parameterize later [X]
+     cmd<=MRS;                                                  //Mode register set command
+     A[2:0]<=3'b000;                                            //Burst legnth is 1. Determines the maximum number of column locations that can be accessed for a given READ or WRITE command
+     A[3]<=1'b0;                                                //Sequencial burst
+     A[6:4]<=3'b010;                                            //Latency mode - CAS is set to 2.The delay, in clock cycles, between a READ command and the availability of the first piece of output data
+     A[8:7]<=2'b00;                                             //Stnadard operation 
+     A[9]<=1'b1;                                                //Write burst mode. '1' for single-location write accesses and '0' for burst length as for wrtie accesses 
+     A[12:10]<=3'b000;                                          //To ensure compatability with futre devices set to '0'
+     BA[1:0]<=2'b00;                                            //To ensure compatability with futre devices set to '0'
+   end
+
+   IDLE: begin
+     if (refresh_flag==1'b1) begin                                    //Refresh is due
+       cmd<=REF;
+       counter_wait<=$bits(counter_wait)'(6);                    //TRC period is 60ns (REF to REF)
+       next_state_after_wait<=IDLE;                 //Two auto refresh commands are required during initialization
+       busy<=1'b1; 
+     end
+    else begin
+      cmd<=NOP;
+      busy<=1'b0;
+    end
+   end
+
+   AUTO_REFRESH: begin
+     cmd<=REF;                                                 //
+     counter_wait<=$bits(counter_wait)'(6);                    //TRC period is 60ns (REF to REF)
+     next_state_after_wait<=IDLE;                              //
+   end
+
+  endcase
+
+//Auto-refresh counter
+always @(posedge i_clk or negedge i_rst)
+  if (!i_rst)
+    counter_rst<=$bits(counter_rst)'(0);
+  else if (counter_rst<$bits(counter_rst)'(782)) begin
+    refresh_flag<=1'b0;
+    counter_rst<=counter_rst+$bits(counter_rst)'(1);
   end
   else begin
-    case (state)
-
-      POWER_DOWN : begin
-        cmd<=DESL;                                                    //Deselects device to minimize power dissipation when memory is not required
-      end
-
-      INITIALIZATION: begin
-        CKE<=1'b1;
-        DQML<=1'b1;
-        DQMH<=1'b1;
-        cmd<=NOP;
-        counter_wait<=$bits(counter_wait)'(XXX);                  //Initialization requires 100us, i.e. X cycles in 100MHz clock
-      end
-
-      Wait: begin
-        counter_wait<=counter_wait-$bits(counter_wait)'(1);       //WAIT state is terminated once the internal counter reaches 0
-      end
-
-    endcase
+     refresh_flag<=1'b1;
+    counter_rst<=$bits(counter_rst)'(0);
   end
 
-
 //Assign the commands
-assign {CS_N,RAS_N,CAS_N,WE_N}=cmd;                               //cmd is synchronized to the positive edge of i_clk
+assign {CS_N,RAS_N,CAS_N,WE_N}=cmd;                            //cmd is synchronized to the positive edge of i_clk
 endmodule 
