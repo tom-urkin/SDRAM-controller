6bb1c01453b9d3e929206e3968e0ce2462638820
tom-urkin <tom.urkin@gmail.com>
2023-06-11 09:08:33 +0300

    Upload source files
    Burst mode enabled

--- SDRAM_Controller_TB.sv
+++ SDRAM_Controller_TB.sv
@@ -3,47 +3,58 @@
 module SDRAM_Controller_TB ();
 
 //Parameters
-parameter A_WIDTH=13;                         //SDRAM address bus length
-parameter BA_WIDTH=2;                         //2^2 memory banks in the SDRAM IC
-parameter D_WIDTH=16;                         //Word length
-parameter A_ROW_WIDTH = 13;                   //Number of rows in each bank
-parameter A_COL_WIDTH = 10;                   //Number of columns in each bank
+parameter A_WIDTH=13;                              //SDRAM address bus length
+parameter BA_WIDTH=2;                              //2^2 memory banks in the SDRAM IC
+parameter D_WIDTH=16;                              //Word length
+parameter A_ROW_WIDTH = 13;                        //Number of rows in each bank
+parameter A_COL_WIDTH = 10;                        //Number of columns in each bank
 
 //Internal signals declarations
-logic i_rst;                                               //Trigerrs the initialization sequence
-logic CLK;                              //SDRAM clock input - 100MHz.
+logic i_rst;                                       //Trigerrs the initialization sequence
+logic CLK;                                         //SDRAM clock input - 100MHz.
 
-logic i_rw;                                                       //'1' for write and '0' for read 
-logic i_initial;                                                  //Rises to logic high to perform read/write
-logic [A_ROW_WIDTH+A_COL_WIDTH-1:0] i_addr;                       //2^13 rows and 2^10 columns
-logic [BA_WIDTH-1:0] i_ba;                                        //Banks select address
-logic [D_WIDTH-1:0] i_data;                                       //Data to be written - [?] [This is a single word -what happens in bursts?] [?]
+logic i_rw;                                        //'1' for write and '0' for read 
+logic [A_WIDTH-1:0] mode_register;                 //
+logic i_initial;                                   //Rises to logic high to perform read/write
+logic [A_ROW_WIDTH+A_COL_WIDTH-1:0] i_addr;        //2^13 rows and 2^10 columns
+logic [BA_WIDTH-1:0] i_ba;                         //Banks select address
+logic [D_WIDTH-1:0] i_data;                        //Data to be written
 
-logic [A_WIDTH-1:0] A;                  //SDRAM address bus
-logic [BA_WIDTH-1:0] BA;                //Banks select address
-logic CKE;                              //Clock enable
-logic CS_N;                             //Chip select
-logic RAS_N;                            //Row address strobe command
-logic CAS_N;                            //Column address strobe command
-logic WE_N;                             //Write enable
-logic DQML;                             //Lower byte, input/output mask
-logic DQMH;                             //Upper byte, input/ouptut mask
+logic [A_WIDTH-1:0] A;                             //SDRAM address bus
+logic [BA_WIDTH-1:0] BA;                           //Banks select address
+logic CKE;                                         //Clock enable
+logic CS_N;                                        //Chip select
+logic RAS_N;                                       //Row address strobe command
+logic CAS_N;                                       //Column address strobe command
+logic WE_N;                                        //Write enable
+logic DQML;                                        //Lower byte, input/output mask
+logic DQMH;                                        //Upper byte, input/ouptut mask
 
-//Fix hold violations
-logic [A_WIDTH-1:0] A_tmp;                  //SDRAM address bus
-logic [BA_WIDTH-1:0] BA_tmp;                //Banks select address
-logic CLK_tmp;                              //SDRAM clock input - 100MHz.
-logic CKE_tmp;                              //Clock enable
-logic CS_N_tmp;                             //Chip select
-logic RAS_N_tmp;                            //Row address strobe command
-logic CAS_N_tmp;                            //Column address strobe command
-logic WE_N_tmp;                             //Write enable
-logic DQM_tmpL;                             //Lower byte, input/output mask
-logic DQMH_tmp;                             //Upper byte, input/ouptut mask
+integer i;                                         //Used in the TB 'for' loops
+integer SEED_a=1;                                  //Used to generate random addresses
+integer SEED_b=10;                                 //Used to generate random bank 
+integer SEED_d=15;                                 //Used to generate random data values to be written into the memory
+logic [A_ROW_WIDTH+A_COL_WIDTH-1:0] addr_tst;      //Address value
+logic [BA_WIDTH-1:0] bank_tst;                     //Bank number
+logic [D_WIDTH-1:0] data_tst;                      //Data to be written into the memory
 
-logic [D_WIDTH-1:0] o_data;             //Word read from memoy. [?] What happens here in bursts? [?]
-logic o_busy;                           //Read/Write operations can be initiated only when not busy, i.e. busy==1'b0
-tri1 [D_WIDTH-1:0] DQ;
+//Fix hold violations - there is a hold time requirement of 800ps so I have added this delay manually
+logic [A_WIDTH-1:0] A_tmp;                         //SDRAM address bus
+logic [BA_WIDTH-1:0] BA_tmp;                       //Banks select address
+logic CLK_tmp;                                     //SDRAM clock input - 100MHz.
+logic CKE_tmp;                                     //Clock enable
+logic CS_N_tmp;                                    //Chip select
+logic RAS_N_tmp;                                   //Row address strobe command
+logic CAS_N_tmp;                                   //Column address strobe command
+logic WE_N_tmp;                                    //Write enable
+logic DQM_tmpL;                                    //Lower byte, input/output mask
+logic DQMH_tmp;                                    //Upper byte, input/ouptut mask
+
+logic [D_WIDTH-1:0] o_data;                        //Word read from memoy
+logic o_busy;                                      //Read/Write operations can be initiated only when not busy, i.e. busy==1'b0
+tri [D_WIDTH-1:0] DQ;                              //DQ bus is of type 'tri' since both the controller and the SDRAM might operate with high-z value
+
+//Modules instantiations
 
 //SDRAM IC instantiation
 IS42S16320f m1(.Dq(DQ),
@@ -59,12 +70,14 @@ IS42S16320f m1(.Dq(DQ),
           );
 
 
-//SDRAM instantiation
+//SDRAM controller instantiation
 SDRAM_controller m0(.i_rst(i_rst),
                     .i_clk(CLK),
+					.i_mode_register(mode_register),
 			        .i_initial(i_initial),
 				    .i_addr(i_addr),
 				    .i_ba(i_ba),
+					.i_data(i_data),
 				    .i_rw(i_rw),
 				    .A(A_tmp),
 				    .BA(BA_tmp),
@@ -81,9 +94,7 @@ SDRAM_controller m0(.i_rst(i_rst),
                     );
 
 
-
-
-//Hold violations fix by adding delay of 0.8ns
+//Hold violations fix by adding delay of 900ps
 assign #0.9 A=A_tmp;
 assign #0.9 BA = BA_tmp;
 assign #0.9 CKE = CKE_tmp;
@@ -94,19 +105,129 @@ assign #0.9 WE_N = WE_N_tmp;
 assign #0.9 DQMH = DQMH_tmp;
 assign #0.9 DQML = DQML_tmp;
 
+
+//Tasks
+//write task recieves address, bank number and data and initiates a 'write' command
+task write(input [A_ROW_WIDTH+A_COL_WIDTH-1:0] address, [BA_WIDTH-1:0] bank, [D_WIDTH-1:0] wr_data);
+  if (o_busy==1'b1) begin
+    $display("\n------------------------------");
+    $display("\nThe controller is busy. Write operration cannot be initiated");   
+	end
+  else begin
+    $display("\n------------------------------");
+    $display("\nInitiating a write command to row number %d, column address %d, bank number %d. The written data is %4h", address[22:10], address[9:0], bank, wr_data);
+    @(posedge CLK)
+    i_initial=1'b1;
+    i_rw=1'b1;
+    i_addr=address;
+    i_ba=bank;
+    i_data=wr_data;
+    @(posedge CLK)
+    i_initial=1'b0;
+	@(negedge o_busy);                                   //Wait until negedge of o_busy which indicates the termination of the write operation
+    compare(addr_tst,bank_tst,data_tst);                 //Verify the correctness of the 'write' operation by executing the 'compare' task	
+  end
+endtask
+
+//'compare' task performs comparison between the data actually written to memory and the data that was inteded to be written
+task compare(input [A_ROW_WIDTH+A_COL_WIDTH-1:0] address, [BA_WIDTH-1:0] bank, [D_WIDTH-1:0] wr_data);
+  case (bank)
+    2'b00: if (m1.Bank0[address]==wr_data) begin
+	  $display("\nData written to row %d and column %d in bank %d matches the stored data in the memory array",address[22:10],address[9:0],bank);	
+	  $display("\nComparison task was succefull!");
+	end
+	else begin
+	  $display("\nComaprison task unfortunately failed. Data stored in memory is: %4h which does not match the written data of %d" , m1.Bank0[address],wr_data);
+	  $finish;	
+	end
+	
+    2'b01: if (m1.Bank1[address]==wr_data) begin
+	  $display("\nData written to row %d and column %d in bank %d matches the stored data in the memory array",address[22:10],address[9:0],bank);	
+	  $display("\nComparison task was succefull!");
+	end
+	else begin
+	  $display("\nComaprison task unfortunately failed. Data stored in memory is: %4h which does not match the written data of %d" , m1.Bank1[address],wr_data);
+	  $finish;	
+	end
+	
+    2'b10: if (m1.Bank2[address]==wr_data) begin
+	  $display("\nData written to row %d and column %d in bank %d matches the stored data in the memory array",address[22:10],address[9:0],bank);	
+	  $display("\nComparison task was succefull!");	
+	end
+	else begin
+	  $display("\nComaprison task unfortunately failed. Data stored in memory is: %4h which does not match the written data of %d" , m1.Bank2[address],wr_data);
+	  $finish;	
+	end
+	
+    2'b11: if (m1.Bank3[address]==wr_data) begin
+	  $display("\nData written to row %d and column %d in bank %d matches the stored data in the memory array",address[22:10],address[9:0],bank);	
+	  $display("\nComparison task was succefull!");	  
+    end	  
+	else begin
+	  $display("\nComaprison task unfortunately failed. Data stored in memory is: %4h which does not match the written data of %d" , m1.Bank3[address],wr_data);
+	  $finish;	
+	end
+  endcase
+endtask
+
+//'read' task...
+task read(input [A_ROW_WIDTH+A_COL_WIDTH-1:0] address, [BA_WIDTH-1:0] bank);
+  if (o_busy==1'b1)
+    $display("\nThe controller is busy. Read operration cannot be initiated");   
+  else begin
+    $display("\nInitiating a read command to row number %d, column address %d, bank number %d", address[22:10], address[9:0], bank);
+    @(posedge CLK)
+    i_initial=1'b1;
+    i_rw=1'b0;
+    i_addr=address;
+    i_ba=bank;
+    @(posedge CLK)
+    i_initial=1'b0;  
+  end
+endtask
+
+//'combined_wr_rd_verification' task...
+task combined_wr_rd_verification(input [A_ROW_WIDTH+A_COL_WIDTH-1:0] address, [BA_WIDTH-1:0] bank, [D_WIDTH-1:0] wr_data);
+	write(addr_tst,bank_tst,data_tst);                            //
+    read(addr_tst,bank_tst);
+endtask
 //Initial blocks
 initial 
 begin
-	i_rst<=1'b0;	
-	CLK<=1'b0;
+	i_rst<=1'b0;                                                  //When i_rst is logic low the SDRAM is in 'power down' mode and the IC is deselected	
+	CLK<=1'b0;                                             
+	i_initial<=1'b0;                                              //Positive edge of i_initial trigerres a read/write operation      
+    mode_register<=13'b0001000100001;	                          //change LSB back to 0 for burst ==1 
 	#1000
-	i_rst<=1'b1;
+	i_rst<=1'b1;                                                  //Logic high for i_rst triggers SDRAM initialization sequence
+	repeat(10050)                                                 //SDRAM initialization sequence requires ~1030 clock cycles.
+	  @(posedge CLK);
+/*
+    for (i=0; i<1; i++) begin
+      addr_tst= $dist_uniform(SEED_a,0,8388607);                  //Generate a random address 
+      bank_tst= $dist_uniform(SEED_b,0,3);                        //Generate a random bank number
+      data_tst= $dist_uniform(SEED_d,0,65535);                    //Generate random word to be written into memory
+      	
+      combined_wr_rd_verification(addr_tst,bank_tst,data_tst);    //Initiate a 'write' command 
+
+      repeat(2)                                                  //Minimum of 2 CLK cycles is required
+	      @(posedge CLK);	    
+    end 
+*/
+
+    $display("\nKAKA");   
+     write(23'd0,2'b00,16'habcd);              //Initiate a 'write' command 
+     //@(negedge o_busy);                                   //Wait until negedge of o_busy which indicates the termination of the write operation
+ 	 write(23'd1,2'b00,16'h1234);              //Initiate a 'write' command 
+     //@(negedge o_busy);                                   //Wait until negedge of o_busy which indicates the termination of the write operation
+     read(23'd0,2'b00);
+	 
 end
 
-//25MHz clock generation
+//100MHz clock generation
 always	
 begin
-#10; 
+#5; 
 CLK=~CLK;
 end		
 
--- wave.do
+++ wave.do
@@ -1,13 +1,23 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
 add wave -noupdate -height 30 -expand -group {TB signals} /SDRAM_Controller_TB/i_rst
+add wave -noupdate /SDRAM_Controller_TB/i_initial
 add wave -noupdate -height 30 -expand -group {Controller signals} /SDRAM_Controller_TB/m0/state
 add wave -noupdate -height 30 -expand -group {Controller signals} /SDRAM_Controller_TB/m0/next_state
-add wave -noupdate -height 30 -expand -group {Controller signals} -radix decimal /SDRAM_Controller_TB/m0/next_state_after_wait
+add wave -noupdate -height 30 -expand -group {Controller signals} -radix unsigned /SDRAM_Controller_TB/m0/next_state_after_wait
 add wave -noupdate -height 30 -expand -group {Controller signals} -radix decimal /SDRAM_Controller_TB/m0/counter_wait
 add wave -noupdate -height 30 -expand -group {Controller signals} -radix unsigned /SDRAM_Controller_TB/m0/counter_rst
 add wave -noupdate -height 30 -expand -group {Controller signals} /SDRAM_Controller_TB/m0/refresh_en
 add wave -noupdate -height 30 -expand -group {Controller signals} /SDRAM_Controller_TB/m0/refresh_flag
+add wave -noupdate /SDRAM_Controller_TB/o_busy
+add wave -noupdate /SDRAM_Controller_TB/o_data
+add wave -noupdate /SDRAM_Controller_TB/DQ
+add wave -noupdate /SDRAM_Controller_TB/i_rw
+add wave -noupdate -radix unsigned /SDRAM_Controller_TB/m0/counter_rd
+add wave -noupdate /SDRAM_Controller_TB/m0/burst
+add wave -noupdate /SDRAM_Controller_TB/m0/latency
+add wave -noupdate /SDRAM_Controller_TB/m0/rd_duration
+add wave -noupdate -radix binary /SDRAM_Controller_TB/m0/A
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/CKE
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/CLK
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/CS_N
@@ -16,12 +26,13 @@ add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Control
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/WE_N
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/m0/DQML
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/m0/DQMH
-add wave -noupdate -height 30 -expand -group {Controller outputs} -radix binary /SDRAM_Controller_TB/A
+add wave -noupdate -height 30 -expand -group {Controller outputs} -radix binary -childformat {{{/SDRAM_Controller_TB/A[12]} -radix binary} {{/SDRAM_Controller_TB/A[11]} -radix binary} {{/SDRAM_Controller_TB/A[10]} -radix binary} {{/SDRAM_Controller_TB/A[9]} -radix binary} {{/SDRAM_Controller_TB/A[8]} -radix binary} {{/SDRAM_Controller_TB/A[7]} -radix binary} {{/SDRAM_Controller_TB/A[6]} -radix binary} {{/SDRAM_Controller_TB/A[5]} -radix binary} {{/SDRAM_Controller_TB/A[4]} -radix binary} {{/SDRAM_Controller_TB/A[3]} -radix binary} {{/SDRAM_Controller_TB/A[2]} -radix binary} {{/SDRAM_Controller_TB/A[1]} -radix binary} {{/SDRAM_Controller_TB/A[0]} -radix binary}} -subitemconfig {{/SDRAM_Controller_TB/A[12]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[11]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[10]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[9]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[8]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[7]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[6]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[5]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[4]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[3]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[2]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[1]} {-height 15 -radix binary} {/SDRAM_Controller_TB/A[0]} {-height 15 -radix binary}} /SDRAM_Controller_TB/A
+add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/m0/o_data
 add wave -noupdate -height 30 -expand -group {Controller outputs} /SDRAM_Controller_TB/m0/BA
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {135918308 ps} 0}
+WaveRestoreCursors {{Cursor 1} {101773688 ps} 0}
 quietly wave cursor active 1
-configure wave -namecolwidth 338
+configure wave -namecolwidth 281
 configure wave -valuecolwidth 100
 configure wave -justifyvalue left
 configure wave -signalnamewidth 0
@@ -35,4 +46,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {59317658 ps} {417930650 ps}
+WaveRestoreZoom {101714446 ps} {101835554 ps}
